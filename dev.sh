cab5e9869530974e79ee984ef76fc57d732b7c88
Christian Baczynski <christian.baczynski@accenture.com>
2026-01-20 13:47:54 +0100

    fix: ghostty config

--- dev.sh
+++ dev.sh
@@ -1,6 +1,11 @@
 WORKSPACE_DIR=/home/christianbaczynski
 NVIM_CONFIG_DIR=$WORKSPACE_DIR/dev-env/nvim_config
 
+# set ghostty config location to current folde
+XDG_CONFIG_HOME=`pwd`
+export XDG_CONFIG_HOME
+printenv 
+
 CONTAINER_EXISTS=`docker container ls -a | grep dev_env:latest`
 if [[ -n "$CONTAINER_EXISTS" ]]; then 
     CONTAINER_ID=`echo $CONTAINER_EXISTS | cut -c1-12`
@@ -10,3 +15,4 @@ else
     echo $NVIM_CONFIG_DIR $WORKSPACE_DIR
     docker run --volume $NVIM_CONFIG_DIR:/root/.config/nvim --volume $WORKSPACE_DIR:/workspace -w /workspace -it dev_env:latest
 fi
+

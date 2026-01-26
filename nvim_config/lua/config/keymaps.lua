864c2b4d67d90ed415b40472e800092a8806076b
Christian <chbaczynski@protonmail.com>
2026-01-26 19:42:53 +0100

    fix: created alias for []

--- nvim_config/lua/config/keymaps.lua
+++ nvim_config/lua/config/keymaps.lua
@@ -1,6 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
---
--- test
--- test
+-- make alias for ] and [ key for German layout
+vim.keymap.set({ "n", "v" }, "ä", "]", { remap = true, desc = "Alias for ]" })
+vim.keymap.set({ "n", "v" }, "ö", "[", { remap = true, desc = "Alias for ]" })
